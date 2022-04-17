2c993654426e65e4637d19de99b11fc5a6c527bb
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 16:59:26 -0500

    primer commit

---
+++ microservicio-retiros-cuentas/src/main/java/nttdata/bootcamp/microservicios/retiros/cuentas/MicroservicioRetirosCuentasApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.retiros.cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioRetirosCuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioRetirosCuentasApplication.class, args);
+	}
+
+}
---
+++ microservicio-retiros-cuentas/src/test/java/nttdata/bootcamp/microservicios/retiros/cuentas/MicroservicioRetirosCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.retiros.cuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioRetirosCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
